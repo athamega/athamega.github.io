0afd5d8fcdd5219f7c71c343ec4eb80c50d714a4
root <root@vultr.guest>
2018-05-17 18:04:40 +0000

    pythonic

--- dry.py
+++ dry.py
@@ -9,7 +9,7 @@ k = 'R_6e7993c0fe4d442aa736b64bfabb9dd3'
 api = bitly.Api(login=l,apikey=k)
 
 import sys
-#import pyimgur
+import pyimgur
 import urllib
 import os
 
@@ -17,15 +17,18 @@ import os
 #im = pyimgur.Imgur('c0c828f140906aa') #4cdfd6f604ab7255792148f5aa9a229732b4b0a1
 #im = pyimgur.Imgur('c0c828f140906aa') #4cdfd6f604ab7255792148f5aa9a229732b4b0a1
 #im = pyimgur.Imgur('b4debe6ca234468') #4cdfd6f604ab7255792148f5aa9a229732b4b0a1
-linkdump=1
-imgdo=1
+
+##im = pyimgur.Imgur('b4debe6ca234468','f5b2c5cde7aabaa9d1083b1c3b668ba67427338f') #f5b2c5cde7aabaa9d1083b1c3b668ba67427338f	
+#yitim = pyimgur.Imgur('220fb4a37cfab8a','f1d4fcce3a0aceebf1c7aab81b2008853340db0b')
+#im = pyimgur.Imgur('c0c828f140906aa','e95813c2e152e6ec1b688635b4b131279dfaa5db')
+im = pyimgur.Imgur('eac405e4fc0208a','2409cc0694ecb6bd755eede32c90dc6fad4d075f')
+imgdo=0
 nolinks=0
 nobitly=0
-if linkdump:
-	ldf = open('/home/adam/ldf.txt','a')
+
 hm = {}
-#keepmasked = ['matchbox20.','www.lamc','/lamc.la','reallyhim.com']
-keepmasked = ['absooluwiolkfdsngkfjgnwefew',]
+keepmasked = ['matchbox20.','www.lamc','/lamc.la','reallyhim.com']
+
 of = sys.argv[1]
 
 text = open(of,'r').read()
@@ -56,15 +59,9 @@ def bysolve2(link):
 	  kmflag=1
   if kmflag==1: 
     return link
-  if 'https://www.google.com/url?q=' in bso.geturl():
-    return bysolve(bso.geturl()[29:])
   if '/x/c?' in bso.geturl():
-    #print "X/C?"
-    url = 'http://gmass.co'+bso.geturl()[bso.geturl().index('/x/c'):]
-    url = url.replace('%3D','=')
-    url = url.replace('%26','&')
-    print url
-    return bysolve(url)
+    print "X/C?"
+    return bysolve(bso.geturl())
   if 'click.php' in bso.geturl():
     print "click.php"
     return bysolve(bso.geturl())
@@ -86,18 +83,19 @@ def bymask(link):
     #print link + " : bymask error"
    return x
 
+if nolinks: print "NO LINKS!!!"
 
 if not nolinks:
  for link in links:
   ##check for bit.ly
 #    print  link['href']
     key = link.get('href')
+    print key
     link['href'] = bysolve(link.get('href'))
     inter = link['href']
     fme=0
     if inter is  None: fme=1 
-#    link['href'] = bymask(link.get('href'))
-    link['href'] = link.get('href')
+    link['href'] = bymask(link.get('href'))
     hm[key]=link['href']
     if fme==0 and key==hm[key] and fme==0:
       #remove  safe-redirect-url and "span id" from google crap
@@ -105,35 +103,24 @@ if not nolinks:
 	print "safered",link
       rid=''.join(random.choice('0123456789ABCDEF') for i in range(16))
       if '?' in inter:
-#	link['href']=bymask(inter+"&"+rid)
+	link['href']=bymask(inter+"&"+rid)
 	hm[key]=link['href']
       else: 
-#	link['href']=bymask(inter+"?"+rid)
+	link['href']=bymask(inter+"?"+rid)
 	hm[key]=link['href']
     if key is None: fme=1
     if fme==0:  key=key.strip()
-    if key=="": key="http://suez.fromthemachine.org"
+    if key=="": key="http://lamc.la"
     print key,inter,hm[key], link.text,
-    l = 'None'
-    if not link['href']:
-      l = 'None'
-    else:
-      l = link['href']
-    if not key: key=''
-    if linkdump:
-      #prit
-      ldf.write(sys.argv[1]+','+key+','+l+'\n')
-    else:
-      if fme==0 and key in link.text and fme==0: link.string.replace(key,link['href'])	
-      if fme==0 and "bit.ly" in link.text and fme==0: link.string=link['href']	
+    if fme==0 and key in link.text and fme==0: link.string.replace(key,link['href'])	
+    if fme==0 and "bit.ly" in link.text and fme==0: link.string=link['href']	
     print link
 #im = pyimgur.Imgur('b4debe6ca234468')
 #im = pyimgur.Imgur('384314be7f1d573')
 #secretda55935876fc9647ee4f0835c6eb183d27719aad
 from PIL import Image
-#if not linkdump for img in imgs: 
-if 0:
- if imgdo==1 and "imgur" not in img.get('src'):
+for img in imgs: 
+ if imgdo==1 and "doltimgur" not in img.get('src'):
   #x = image.retrieve(img.get('src'),'tempimg')
   #print x
   rid=''.join(random.choice('0123456789ABCDEF') for i in range(30000))
